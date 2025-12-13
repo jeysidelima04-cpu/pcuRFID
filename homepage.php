6050cc7aafa579ec4d2e508cf97f707aa8dd3120
jeysidelima04-cpu <jeysidelima04@gmail.com>
2025-12-13 13:41:16 +0800

    Remove camera button for profile picture upload from student homepage

--- homepage.php
+++ homepage.php
@@ -564,14 +564,6 @@ try {
                             <?= htmlspecialchars($firstLetter) ?>
                         </div>
                     <?php endif; ?>
-                    
-                    <!-- Upload/Change Picture Button (always visible) -->
-                    <button onclick="openProfilePictureModal()" class="absolute bottom-0 right-0 w-10 h-10 bg-sky-600 hover:bg-sky-700 rounded-full flex items-center justify-center shadow-lg border-2 border-white transition-all duration-200 hover:scale-110" title="Upload Photo">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                    </button>
                 </div>
 
                 <!-- Welcome Message -->
